fbbfaff5ed157e8e468b6ed0c4a73383b8215599
alokranjan04 <alokranjan04@gmail.com>
2024-12-20 02:08:09 +0530

    fixing build issues

--- app/api/sentry-example-api/route.ts
+++ app/api/sentry-example-api/route.ts
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
+import { NextResponse } from "next/server";
 
 // A faulty API route to test Sentry's error monitoring
 export function GET() {
-  throw new Error("Sentry Example API Route Error");
+  return NextResponse.json({ data: "Testing Sentry Error..." });
 }
